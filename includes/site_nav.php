a907371b9b0bb84a10593bba3f90fa8637cb7505
chhotuthakur <npsdahiya@gmail.com>
2022-04-25 18:27:29 +0530

    updated haeder

--- includes/site_nav.php
+++ includes/site_nav.php
@@ -5,12 +5,26 @@
               <div class="container-fluid" style="background-color: darkred;" >
                   <div class="row">
 
-                    <div class="col-sm-6 col-xs-12">
+                    <div class="col" width="18%"  style="display: inline-block;" >
+                    <ul class="list-unstyled list-inline header-contact " width="18%">
+                            <li> <img src="assets/images/logo.png" width="100px" height="auto" mb-0  pb-0 style="float: inline-start;"> <a href="index.php"></a> </li>
+                    </ul>
+                    </div>
+                    <div class ="col"    > 
 
-                        <ul class="list-unstyled list-inline header-contact">
-                            <li> <img src="assets/images/logo.png" width="75px" height="auto" mb-2> <a href="index.php"></a> </li>
-                             <li></i><h3> <a href="index.php">JAI SHRI RADHEY KRISHN FOUNDATION</a></h3>
+                        <ul class="list-unstyled list-inline header-contact" >
+                            <!-- <li> <img src="assets/images/logo.png" width="100px" height="auto" mb-0  pb-0 style="float: inline-start;"> <a href="index.php"></a> </li> -->
+                            
+                           <span>
+                            <li></i><h3 style="float: inline-end; margin-bottom :0px; " > <a href="index.php">JAI SHRI RADHEY KRISHN FOUNDATION</a></h3><br>
+</span>
+                            <span> 
+                            <li><div>
+
+                      <div class="squareleft" style="text-align: center; display:flex; flex-direction: column; align-items: right"></div><p class="subtitle" >sabke Sath</p><div class="squareright"></div></div></li>
                             </li>
+                           </span>
+                            </div>
                        </ul> <!-- /.header-contact  -->
                       
                     </div>
